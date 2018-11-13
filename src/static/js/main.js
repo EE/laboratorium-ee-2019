8253c541695894fcf87f09c427acfee3f7875c73
Pawel Lulinski <pawel.lulinski@laboratorium.ee>
2018-11-13 12:36:42 +0100

    experiment with vue

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,1 +1,10 @@
 /* Write your ES6 here. Feel free to use imports/exports. */
+window.addEventListener('load', function () {
+    Vue.options.delimiters = ['[[', ']]'];
+     var app = new Vue({
+         el: '#app',
+         data: {
+             message: 'Hello, world!'
+         }
+     });
+})
