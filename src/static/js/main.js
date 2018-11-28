f4a4b25720233514749bcd70f9e4d39e3209f6af
Pawel Lulinski <pawel.lulinski@laboratorium.ee>
2018-11-28 11:55:07 +0100

    js lint

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,10 +1,13 @@
 /* Write your ES6 here. Feel free to use imports/exports. */
-window.addEventListener('load', function () {
-    Vue.options.delimiters = ['[[', ']]'];
-     var app = new Vue({
-         el: '#app',
-         data: {
-             message: 'Hello, world!'
-         }
-     });
-})
+/* eslint-disable no-unused-vars */
+
+window.addEventListener("load", function () {
+    Vue.options.delimiters = ["[[", "]]"];
+    const app = new Vue({
+        el: "#app",
+        data: {
+            message: "Hello, world!"
+        }
+    });
+});
+/* eslint-enable no-unused-vars */
