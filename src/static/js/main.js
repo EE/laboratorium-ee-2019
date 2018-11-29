67cfceb118f6075f66e8cc7fa1bb16339d6c9883
plulee <32864746+plulee@users.noreply.github.com>
2018-11-29 12:39:32 +0100

    Bulma (#1)
    * experiment with vue
    
    * vue tests
    
    * move babel-preset-vue from dev dependencies to dependencies
    
    * added integrity and crossorgin attributes to vue cdn
    
    * js lint
    
    * lint scss
    
    * eslint no-unused-vars disable one line instead of in whole file
    
    * updated package-lock.json

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,1 +1,11 @@
 /* Write your ES6 here. Feel free to use imports/exports. */
+
+window.addEventListener("load", function () {
+    Vue.options.delimiters = ["[[", "]]"];
+    const app = new Vue({ // eslint-disable-line no-unused-vars
+        el: "#app",
+        data: {
+            message: "Hello, world!"
+        }
+    });
+});
