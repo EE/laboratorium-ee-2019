f20e4fbfc1d7bb54e461245f176249f69169815d
Jan Rydzewski <SupraSummus@users.noreply.github.com>
2018-12-03 11:25:49 +0100

    Configure sentry integration for frontend (#9)

--- src/settings/deployment_test.py
+++ src/settings/deployment_test.py
@@ -21,6 +21,7 @@ RAVEN_CONFIG = {
     'transport': RequestsHTTPTransport
 }
 INSTALLED_APPS += ('raven.contrib.django.raven_compat',)
+DSN_PUBLIC = 'https://194ce8ca6d7441599d3c1c62c9f39e1d@sentry.laboratorium.ee/30'
 
 
 # staticfile serving
--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,4 +1,11 @@
-/* Write your ES6 here. Feel free to use imports/exports. */
+import * as sentry from "@sentry/browser";
+
+const DSN_PUBLIC = document.getElementById("DSN_PUBLIC").textContent;
+if (DSN_PUBLIC) {
+    sentry.init({
+        dsn: DSN_PUBLIC,
+    });
+}
 
 window.addEventListener("load", function () {
     Vue.options.delimiters = ["[[", "]]"];
