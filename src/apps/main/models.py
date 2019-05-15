191d2fc8dc61970984f4529ae88521d60046228d
Jan Rydzewski <flegmer@gmail.com>
2019-05-15 11:22:07 +0200

    Specializations displayed on homepage

--- src/apps/main/models.py
+++ src/apps/main/models.py
@@ -3,6 +3,8 @@ from django.db import models
 from wagtail.core.models import Page
 from wagtail.admin.edit_handlers import FieldPanel
 
+from src.apps.projects.models import SpecializationPage
+
 
 class HomePage(Page):
     header = models.CharField(max_length=255)
@@ -10,3 +12,7 @@ class HomePage(Page):
     content_panels = Page.content_panels + [
         FieldPanel('header'),
     ]
+
+    @property
+    def specializations(self):
+        return SpecializationPage.objects.live().descendant_of(self)
