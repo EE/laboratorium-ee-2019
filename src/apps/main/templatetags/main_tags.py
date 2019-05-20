76b9307b423c28151d6d9b773e70514bc0eaaeb8
Kamil Radomski <kamilmarekradomski@gmail.com>
2019-05-20 16:11:47 +0200

    Move upper menu to templat tags (#35)

--- src/apps/main/templatetags/main_tags.py
+++ src/apps/main/templatetags/main_tags.py
@@ -13,3 +13,10 @@ def contact_form(context):
 @register.inclusion_tag('email_with_attachment_form.html', takes_context=True)
 def contact_form_with_attachment(context):
     return {'form': AttachmentContactForm(request=context.get('request'))}
+
+
+@register.inclusion_tag('main/menu.html', takes_context=True)
+def upper_menu(context):
+    return {
+        'menus': context['request'].site.root_page.get_descendants().live().in_menu(),
+    }
