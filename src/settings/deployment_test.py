5fe286b14729c245f9ed2badd031af38dd732930
Jan Rydzewski <flegmer@gmail.com>
2019-05-28 12:22:13 +0200

    Configuring S3 mediafiles storage

--- src/settings/deployment_test.py
+++ src/settings/deployment_test.py
@@ -26,3 +26,17 @@ sentry_sdk.init(
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 CONTACT_EMAIL = ''
+
+# mediafiles on AWS S3
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_ACCESS_KEY_ID = 'AKIAZ64OAD7GVKVUGVEY'
+AWS_SECRET_ACCESS_KEY = env.str('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = 'strona-ee-test'
+AWS_DEFAULT_ACL = 'public-read'
+AWS_BUCKET_ACL = 'public-read'
+AWS_QUERYSTRING_AUTH = False
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_LOCATION = 'media'
