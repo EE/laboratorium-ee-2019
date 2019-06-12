9c170e2c7d5af205f7a3574cc675bb1b3e6cb1b9
Jan Rydzewski <flegmer@gmail.com>
2019-06-12 11:19:11 +0200

    use minified bulma-carousel source

--- src/static/src/js/carousel.js
+++ src/static/src/js/carousel.js
@@ -1,4 +1,4 @@
-import bulmaCarousel from "bulma-carousel/src/js/index";
+import bulmaCarousel from "bulma-carousel/dist/js/bulma-carousel.min";
 
 bulmaCarousel.attach(".hero-carousel", {
     slidesToScroll: 1,
