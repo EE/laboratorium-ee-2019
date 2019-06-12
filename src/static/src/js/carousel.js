9d219ab3db23a07503dabdff5afd014ee5fefd39
Jan Rydzewski <flegmer@gmail.com>
2019-06-12 10:52:23 +0200

    hero slider not working

--- src/static/src/js/carousel.js
+++ src/static/src/js/carousel.js
@@ -1,15 +1,14 @@
 import bulmaCarousel from 'bulma-carousel/src/js/index';
 
-const carousels = bulmaCarousel.attach('.carousel', {
+const carousels = bulmaCarousel.attach('.hero-carousel', {
     slidesToScroll: 1,
     slidesToShow: 1,
-    effect: 'fade',
 });
 
-carousels.forEach(carousel => {
+/*carousels.forEach(carousel => {
     carousel.on('show', event => {
         //const backgroundImageUrl = event.element.
         console.log(event.element);
         
     });
-});
+});*/
