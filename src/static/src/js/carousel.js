56dcd199c860986d17d8abaecf4effd8ddd385a1
Jan Rydzewski <flegmer@gmail.com>
2019-06-12 11:06:32 +0200

    slider for images only

--- src/static/src/js/carousel.js
+++ src/static/src/js/carousel.js
@@ -1,14 +1,6 @@
-import bulmaCarousel from 'bulma-carousel/src/js/index';
+import bulmaCarousel from "bulma-carousel/src/js/index";
 
-const carousels = bulmaCarousel.attach('.hero-carousel', {
+bulmaCarousel.attach(".hero-carousel", {
     slidesToScroll: 1,
     slidesToShow: 1,
 });
-
-/*carousels.forEach(carousel => {
-    carousel.on('show', event => {
-        //const backgroundImageUrl = event.element.
-        console.log(event.element);
-        
-    });
-});*/
