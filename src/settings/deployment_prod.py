97e7929126f7763ce7119b8cd31e241294d5311c
Jan Rydzewski <SupraSummus@users.noreply.github.com>
2019-06-14 09:55:04 +0200

    Initial settings for production server (#64)

---
+++ src/settings/deployment_prod.py
@@ -0,0 +1,8 @@
+"""Settings for prodution server (not deployed yet)."""
+
+# ignore the 'may be undefined, or defined from star imports' error
+# flake8: noqa: F999
+
+from base import *  # noqa
+
+GOOGLE_ANALYTICS_ID = 'UA-86468922-1'
