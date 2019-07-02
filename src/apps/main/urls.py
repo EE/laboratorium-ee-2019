dc11933b63c3bca60fbef2217bd602ba9b939345
Jan Rydzewski <SupraSummus@users.noreply.github.com>
2019-07-02 13:46:18 +0200

    Remove recruitment-specific contact form (#125)

--- src/apps/main/urls.py
+++ src/apps/main/urls.py
@@ -9,11 +9,6 @@ urlpatterns = [
         name='send_mail'
     ),
     url(
-        r'^recruitment-contact-form/$',
-        views.RecruitmentContactView.as_view(),
-        name='recruitment_contact'
-    ),
-    url(
         r'^thanks/$',
         views.SendMailSuccessView.as_view(),
         name='send_mail_done',
