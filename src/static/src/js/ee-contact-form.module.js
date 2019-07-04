88ccf9880dc8219fbf44c8e58e14ae74eae69881
Jan Rydzewski <flegmer@gmail.com>
2019-07-04 13:32:01 +0200

    Contat form accessible via contact buttons

--- src/static/src/js/ee-contact-form.module.js
+++ src/static/src/js/ee-contact-form.module.js
@@ -1,9 +1,17 @@
 (function () {
     const contactFormToggle = document.querySelector("[data-toggle='contactForm']");
+    const getFormSticky = () => document.querySelector(".ee-sticky-contact-form");
 
     if (contactFormToggle) {
         contactFormToggle.addEventListener("click", () => {
-            document.querySelector(".ee-sticky-contact-form").classList.toggle("active");
+            getFormSticky().classList.toggle("active");
+        });
+
+        // buttons for opening the sidebar
+        [...document.querySelectorAll(".ee-contact-us-button").values()].forEach(el => {
+            el.addEventListener("click", () => {
+                getFormSticky().classList.add("active");
+            });
         });
     }
 })();
