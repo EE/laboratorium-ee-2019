9e431af56f7671fa34fc21461801e569788e4907
Piotr Wegner <piotr.wegner@live.com>
2019-07-04 10:28:20 +0200

    Subpages

--- src/static/src/js/main.js
+++ src/static/src/js/main.js
@@ -3,6 +3,7 @@ import "../scss/main.scss";
 import "./sentry";
 import "./navbar";
 import "./contact_form";
+import "./ee-contact-form.module";
 import "./ee-custom-carousel.module";
 import "./ee-navbar.module";
 import "./ee-solutions.module";
