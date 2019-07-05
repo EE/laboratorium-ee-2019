a7a19e6363d9ad166c932926f83e900efe68cee4
Jan Rydzewski <flegmer@gmail.com>
2019-07-05 09:59:45 +0200

    Refactor contact form buttons to use data-toggle

--- src/static/src/js/ee-contact-form.module.js
+++ src/static/src/js/ee-contact-form.module.js
@@ -1,17 +1,10 @@
 (function () {
-    const contactFormToggle = document.querySelector("[data-toggle='contactForm']");
     const getFormSticky = () => document.querySelector(".ee-sticky-contact-form");
 
-    if (contactFormToggle) {
-        contactFormToggle.addEventListener("click", () => {
+    // buttons for opening the sidebar
+    [...document.querySelectorAll("[data-toggle='contactForm']").values()].forEach(el => {
+        el.addEventListener("click", () => {
             getFormSticky().classList.toggle("active");
         });
-
-        // buttons for opening the sidebar
-        [...document.querySelectorAll(".ee-contact-us-button").values()].forEach(el => {
-            el.addEventListener("click", () => {
-                getFormSticky().classList.add("active");
-            });
-        });
-    }
+    });
 })();
