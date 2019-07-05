7aee61bc285f6a32e27f78888c7fd1b8cb5eb1e6
Piotr Wegner <piotr.wegner@live.com>
2019-07-05 17:56:23 +0200

    Contact form fix

--- src/static/src/js/ee-contact-form.module.js
+++ src/static/src/js/ee-contact-form.module.js
@@ -2,9 +2,40 @@
     const getFormSticky = () => document.querySelector(".ee-sticky-contact-form");
 
     // buttons for opening the sidebar
-    [...document.querySelectorAll("[data-toggle='contactForm']").values()].forEach(el => {
+    [ ...document.querySelectorAll("[data-toggle='contactForm']").values() ].forEach(el => {
         el.addEventListener("click", () => {
             getFormSticky().classList.toggle("active");
         });
+    });
+
+    document.querySelector(".ee-sticky-contact-form__close-button").addEventListener("click", () => {
+        getFormSticky().classList.remove("active");
+    });
+
+    [ ...document.querySelectorAll(".ee-form-control").values() ].forEach(el => {
+        const input = el.querySelector("input[type='text']");
+        const file = el.querySelector("input[type='file']");
+        const textarea = el.querySelector("textarea");
+        if (input) {
+            input.addEventListener("change", e => {
+                if (input.value) {
+                    el.querySelector("label").classList.add("active");
+                } else {
+                    el.querySelector("label").classList.remove("active");
+                }
+            });
+        }
+        if (file) {
+            el.querySelector("label").classList.add("active");
+        }
+        if (textarea) {
+            textarea.addEventListener("change", e => {
+                if (textarea.value) {
+                    el.querySelector("label").classList.add("active");
+                } else {
+                    el.querySelector("label").classList.remove("active");
+                }
+            });
+        }
     });
 })();
