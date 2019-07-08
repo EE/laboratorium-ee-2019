cfebe90801cd031af6b7a912b2c1ff9141141711
Piotr Wegner <piotr.wegner@live.com>
2019-07-08 17:27:28 +0200

    Various fixes

--- src/static/src/js/ee-custom-carousel.module.js
+++ src/static/src/js/ee-custom-carousel.module.js
@@ -32,7 +32,7 @@ import * as Hammer from "hammerjs";
                     [ ...carouselControlsButtons.values() ].forEach((el) => {
                         el.classList.remove("active");
                     });
-                    [...carouselControlsButtons.values()][idx + 1].classList.add("active");
+                    [ ...carouselControlsButtons.values() ][ idx + 1 ].classList.add("active");
                 }
             });
             carousel.on("swiperight", () => {
@@ -41,7 +41,7 @@ import * as Hammer from "hammerjs";
                     [ ...carouselControlsButtons.values() ].forEach((el) => {
                         el.classList.remove("active");
                     });
-                    [...carouselControlsButtons.values()][idx - 1].classList.add("active");
+                    [ ...carouselControlsButtons.values() ][ idx - 1 ].classList.add("active");
                 }
             });
         });
@@ -54,6 +54,60 @@ import * as Hammer from "hammerjs";
                     el.classList.remove("active");
                 });
                 el.classList.add("active");
+            });
+        });
+    }
+})();
+
+(function () {
+    const SWIPE_DIRECTION = { left: 2, right: 4 };
+    const secondaryCarousels = [ ...document.querySelectorAll(".has-slider").values() ];
+
+    if (secondaryCarousels.length > 0) {
+        secondaryCarousels.forEach(secondaryCarousel => {
+            const slidesContainer = secondaryCarousel.querySelector(".is-slides-container");
+            const slides = [ ...slidesContainer.querySelectorAll(".is-slide").values() ];
+            const slidesControls = secondaryCarousel.querySelector(".ee-section__slides-controls");
+
+
+            slides.forEach((el, idx, arr) => {
+                const carousel = new Hammer(el);
+                const defaultActive = idx === 0 ? "active" : null;
+                const controlButton = document.createElement("div");
+                controlButton.classList.add("ee-section__slide-control-button");
+                if (defaultActive) {
+                    controlButton.classList.add(defaultActive);
+                }
+                slidesControls.appendChild(controlButton);
+
+                carousel.get("swipe").set({ direction: Hammer.DIRECTION_HORIZONTAL });
+                carousel.get("pan").set({ enabled: false });
+
+                carousel.on("swipe", (e) => {
+                    switch (e.direction) {
+                    case SWIPE_DIRECTION.left:
+                        if (idx < arr.length - 1) {
+                            slidesContainer.style.transform = `translateX(-${(idx + 1) * 100}vw)`;
+                            [ ...slidesControls.querySelectorAll(".ee-section__slide-control-button").values() ].forEach((el) => {
+                                el.classList.remove("active");
+                            });
+                            [ ...slidesControls.querySelectorAll(".ee-section__slide-control-button").values() ][ idx + 1 ].classList.add("active");
+                        }
+                        break;
+                    case SWIPE_DIRECTION.right:
+                        if (idx > 0) {
+                            slidesContainer.style.transform = `translateX(-${(idx - 1) * 100}vw)`;
+                            [ ...slidesControls.querySelectorAll(".ee-section__slide-control-button").values() ].forEach((el) => {
+                                el.classList.remove("active");
+                            });
+                            [ ...slidesControls.querySelectorAll(".ee-section__slide-control-button").values() ][ idx - 1 ].classList.add("active");
+                        }
+                        break;
+                    default:
+                        break;
+                    }
+                });
+
             });
         });
     }
