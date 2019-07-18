889f0ad4d0e2aa5f526b0f127c2b2eae50a40773
Jan Rydzewski <flegmer@gmail.com>
2019-07-18 12:24:27 +0200

    Fix indents and sentry init triggering

--- src/static/src/js/sentry.js
+++ src/static/src/js/sentry.js
@@ -1,10 +1,8 @@
 import * as sentry from "@sentry/browser";
 
-document.addEventListener("DOMContentLoaded", () => {
-    const SENTRY_DSN = document.getElementById("SENTRY_DSN").textContent;
-    if (SENTRY_DSN) {
-        sentry.init({
-            dsn: SENTRY_DSN,
-        });
-    }
-});
+const SENTRY_DSN = document.getElementById("SENTRY_DSN").textContent;
+if (SENTRY_DSN) {
+    sentry.init({
+        dsn: SENTRY_DSN,
+    });
+}
