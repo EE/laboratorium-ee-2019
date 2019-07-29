e3abd0ac2ccfe65e082c9f771d7cefce956d50b5
Jan Rydzewski <flegmer@gmail.com>
2019-07-29 12:40:15 +0200

    Dynamic img 'sizes' attribute

--- ee_site/static/src/js/lazy_image.js
+++ ee_site/static/src/js/lazy_image.js
@@ -1,28 +1,40 @@
 // lazy image loader based on
 // https://developers.google.com/web/fundamentals/performance/lazy-loading-guidance/images-and-video/#inline_images
-document.addEventListener("DOMContentLoaded", () => {
+(function () {
     if ("IntersectionObserver" in window) {
+
         const lazyImageObserver = new IntersectionObserver((entries, observer) => {
             entries.forEach(entry => {
                 if (entry.isIntersecting) {
                     const lazyImage = entry.target;
+
+                    // set "sizes" to current display size
+                    lazyImage.sizes = `${lazyImage.clientWidth}px`;
+
+                    // assign "srcset" allowing to load perfectly sized image
                     lazyImage.srcset = lazyImage.dataset.srcset;
-                    lazyImage.classList.remove("lazy");
+
+                    // no need to watch this image anymore (until next resize event)
                     observer.unobserve(lazyImage);
                 }
             });
         });
 
-        [...document.querySelectorAll("img.lazy").values()].forEach(lazyImage => {
-            lazyImageObserver.observe(lazyImage);
-        });
+        // observe images after every resize
+        const observeImages = () => {
+            [...document.querySelectorAll("img.lazy").values()].forEach(lazyImage => {
+                lazyImageObserver.observe(lazyImage);
+            });
+        };
+        window.addEventListener("resize", observeImages);
+        observeImages();
 
     } else {
         // Fall back to a more compatible method here (IntersectionObserver is sometimes not supported).
-        // Just load all images.
+        // Just load all images assuming default "sizes" attribute.
         [...document.querySelectorAll("img.lazy").values()].forEach(lazyImage => {
             lazyImage.srcset = lazyImage.dataset.srcset;
         });
 
     }
-});
+})();
