d44c4417cb0c604d3f7a515634984c9c32c1815d
Jan Rydzewski <flegmer@gmail.com>
2019-07-31 10:12:08 +0200

    remove js wrapper function

--- ee_site/static/src/js/lazy_image.js
+++ ee_site/static/src/js/lazy_image.js
@@ -1,40 +1,39 @@
 // lazy image loader based on
 // https://developers.google.com/web/fundamentals/performance/lazy-loading-guidance/images-and-video/#inline_images
-(function () {
-    if ("IntersectionObserver" in window) {
 
-        const lazyImageObserver = new IntersectionObserver((entries, observer) => {
-            entries.forEach(entry => {
-                if (entry.isIntersecting) {
-                    const lazyImage = entry.target;
+if ("IntersectionObserver" in window) {
 
-                    // set "sizes" to current display size
-                    lazyImage.sizes = `${lazyImage.clientWidth}px`;
+    const lazyImageObserver = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                const lazyImage = entry.target;
 
-                    // assign "srcset" allowing to load perfectly sized image
-                    lazyImage.srcset = lazyImage.dataset.srcset;
+                // set "sizes" to current display size
+                lazyImage.sizes = `${lazyImage.clientWidth}px`;
 
-                    // no need to watch this image anymore (until next resize event)
-                    observer.unobserve(lazyImage);
-                }
-            });
+                // assign "srcset" allowing to load perfectly sized image
+                lazyImage.srcset = lazyImage.dataset.srcset;
+
+                // no need to watch this image anymore (until next resize event)
+                observer.unobserve(lazyImage);
+            }
         });
+    });
 
-        // observe images after every resize
-        const observeImages = () => {
-            [...document.querySelectorAll("img.lazy").values()].forEach(lazyImage => {
-                lazyImageObserver.observe(lazyImage);
-            });
-        };
-        window.addEventListener("resize", observeImages);
-        observeImages();
-
-    } else {
-        // Fall back to a more compatible method here (IntersectionObserver is sometimes not supported).
-        // Just load all images assuming default "sizes" attribute.
+    // observe images after every resize
+    const observeImages = () => {
         [...document.querySelectorAll("img.lazy").values()].forEach(lazyImage => {
-            lazyImage.srcset = lazyImage.dataset.srcset;
+            lazyImageObserver.observe(lazyImage);
         });
+    };
+    window.addEventListener("resize", observeImages);
+    observeImages();
 
-    }
-})();
+} else {
+    // Fall back to a more compatible method here (IntersectionObserver is sometimes not supported).
+    // Just load all images assuming default "sizes" attribute.
+    [...document.querySelectorAll("img.lazy").values()].forEach(lazyImage => {
+        lazyImage.srcset = lazyImage.dataset.srcset;
+    });
+
+}
