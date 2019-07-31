34063ec3a9d7e6983dc82e5bc17ad95c76b49504
Piotr Wegner <piotr.wegner@live.com>
2019-08-01 00:47:36 +0200

    SEN-523 Contact Form

--- ee_site/static/src/js/ee-contact-form.module.js
+++ ee_site/static/src/js/ee-contact-form.module.js
@@ -1,82 +1,85 @@
 (function () {
     const getFormSticky = () => document.querySelector(".ee-sticky-contact-form");
 
-    // buttons for opening the sidebar
-    [ ...document.querySelectorAll("[data-toggle='contactForm']").values() ].forEach(el => {
-        el.addEventListener("click", () => {
-            getFormSticky().classList.toggle("active");
+    if(getFormSticky()) {
+
+        // buttons for opening the sidebar
+        [ ...document.querySelectorAll("[data-toggle='contactForm']").values() ].forEach(el => {
+            el.addEventListener("click", () => {
+                getFormSticky().classList.toggle("active");
+            });
         });
-    });
 
-    const closeButton = document.querySelector(".ee-sticky-contact-form__close-button");
-    if (closeButton) closeButton.addEventListener("click", () => {
-        getFormSticky().classList.remove("active");
-    });
+        const closeButton = document.querySelector(".ee-sticky-contact-form__close-button");
+        if (closeButton) closeButton.addEventListener("click", () => {
+            getFormSticky().classList.remove("active");
+        });
 
-    [ ...document.querySelectorAll(".ee-form-control").values() ].forEach(el => {
-        const input = el.querySelector("input[type='text']");
-        const file = el.querySelector("input[type='file']");
-        const textarea = el.querySelector("textarea");
-        if (input) {
-            input.addEventListener("change", () => {
-                if (input.value) {
-                    el.querySelector("label").classList.add("active");
-                } else {
-                    el.querySelector("label").classList.remove("active");
-                }
-            });
-        }
-        if (file) {
-            el.querySelector("label").classList.add("active");
-        }
-        if (textarea) {
-            textarea.addEventListener("change", () => {
-                if (textarea.value) {
-                    el.querySelector("label").classList.add("active");
-                } else {
-                    el.querySelector("label").classList.remove("active");
-                }
-            });
-        }
-    });
+        [ ...document.querySelectorAll(".ee-form-control").values() ].forEach(el => {
+            const input = el.querySelector("input[type='text']");
+            const file = el.querySelector("input[type='file']");
+            const textarea = el.querySelector("textarea");
+            if (input) {
+                input.addEventListener("change", () => {
+                    if (input.value) {
+                        el.querySelector("label").classList.add("active");
+                    } else {
+                        el.querySelector("label").classList.remove("active");
+                    }
+                });
+            }
+            if (file) {
+                el.querySelector("label").classList.add("active");
+            }
+            if (textarea) {
+                textarea.addEventListener("change", () => {
+                    if (textarea.value) {
+                        el.querySelector("label").classList.add("active");
+                    } else {
+                        el.querySelector("label").classList.remove("active");
+                    }
+                });
+            }
+        });
 
-    /* conditional showing form fields */
-    [...document.querySelectorAll(".ee-contact-form").values()].forEach(form => {
+        /* conditional showing form fields */
+        [ ...document.querySelectorAll(".ee-contact-form").values() ].forEach(form => {
 
-        const formField = name => form.querySelector(`*[name=${name}]`);
+            const formField = name => form.querySelector(`*[name=${name}]`);
 
-        const hide = name => {
-            formField(name).closest(".ee-form-control").style.display = "none";
-        };
-        const show = name => {
-            formField(name).closest(".ee-form-control").style.display = "block";
-        };
-        const noop = () => undefined;
+            const hide = name => {
+                formField(name).closest(".ee-form-control").style.display = "none";
+            };
+            const show = name => {
+                formField(name).closest(".ee-form-control").style.display = "block";
+            };
+            const noop = () => undefined;
 
-        const updateForm = () => {
-            hide("organization_name");
-            hide("recruitment_position");
-            hide("attachment");
-            hide("recruitment_terms_accepted");
+            const updateForm = () => {
+                hide("organization_name");
+                hide("recruitment_position");
+                hide("attachment");
+                hide("recruitment_terms_accepted");
 
-            const subject = formField("subject").value;
-            if (subject === "") return;  // no subject chosen yet
-            ({
-                offer: () => {
-                    show("organization_name");
-                },
-                recruitment: () => {
-                    show("recruitment_position");
-                    show("attachment");
-                    show("recruitment_terms_accepted");
-                },
-                other: noop,
-            })[subject]();
-        };
+                const subject = formField("subject").value;
+                if (subject === "") return;  // no subject chosen yet
+                ({
+                    offer: () => {
+                        show("organization_name");
+                    },
+                    recruitment: () => {
+                        show("recruitment_position");
+                        show("attachment");
+                        show("recruitment_terms_accepted");
+                    },
+                    other: noop,
+                })[ subject ]();
+            };
 
-        const subjectField = formField("subject");
-        subjectField.addEventListener("change", updateForm);
-        updateForm();
-    });
+            const subjectField = formField("subject");
+            subjectField.addEventListener("change", updateForm);
+            updateForm();
+        });
 
+    }
 })();
--- ee_site/static/src/js/main.js
+++ ee_site/static/src/js/main.js
@@ -1,10 +1,13 @@
 import "../scss/main.scss";
 
+import "material-design-lite/material";
 import "./sentry";
 
 import "./dynamic_fill_image";
 import "./ee-contact-form.module";
+import "./ee-contact-page.module";
 import "./ee-custom-carousel.module";
+import "./ee-mdl-overrides.module";
 import "./ee-navbar.module";
 import "./ee-parallax.module";
 import "./ee-solutions.module";
