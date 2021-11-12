dd69ad21b8b6033fe17c949cc7dd96720a652730
Jan Rydzewski <flegmer@gmail.com>
2021-11-12 14:26:43 +0100

    Configure ee standard linters

--- ee_site/static/src/js/ee-mdl-overrides.module.js
+++ ee_site/static/src/js/ee-mdl-overrides.module.js
@@ -1,13 +1,15 @@
-[...document.querySelectorAll(".ee-contact-form__attachment")].forEach(attachmentField => {
+[...document.querySelectorAll(".ee-contact-form__attachment")].forEach(
+  (attachmentField) => {
     const fileUploadButton = attachmentField.querySelector("input[type=file]");
     const fileUploadInput = attachmentField.querySelector("input[type=text]");
 
     // display selected file's name
     fileUploadButton.addEventListener("change", function () {
-        fileUploadInput.value = this.files[ 0 ].name;
+      fileUploadInput.value = this.files[0].name;
     });
 
     fileUploadInput.addEventListener("click", () => {
-        fileUploadButton.click();
+      fileUploadButton.click();
     });
-});
+  },
+);
--- ee_site/static/src/js/ee-parallax.module.js
+++ ee_site/static/src/js/ee-parallax.module.js
@@ -1,23 +1,25 @@
 (function () {
-    document.addEventListener("scroll", () => {
-        [ ...document.querySelectorAll(".ee-has-parallax").values() ].forEach(el => {
-            parallaxFx(el);
-        });
-    });
+  document.addEventListener("scroll", () => {
+    [...document.querySelectorAll(".ee-has-parallax").values()].forEach(
+      (el) => {
+        parallaxFx(el);
+      },
+    );
+  });
 
-    function parallaxFx(el) {
-        const scrollY = el.getBoundingClientRect().top;
-        const viewportHeight = window.innerHeight;
+  function parallaxFx(el) {
+    const scrollY = el.getBoundingClientRect().top;
+    const viewportHeight = window.innerHeight;
 
-        if (scrollY > (viewportHeight / 2 * -1) && scrollY < viewportHeight) {
-            const y = scrollY / 11;
+    if (scrollY > (viewportHeight / 2) * -1 && scrollY < viewportHeight) {
+      const y = scrollY / 11;
 
-            // support CSS background
-            el.style.backgroundPositionY = `${y}px`;
+      // support CSS background
+      el.style.backgroundPositionY = `${y}px`;
 
-            // support img tag background
-            const imageElement = el.querySelector(".ee-background-image");
-            if (imageElement) imageElement.style.top = `${y}px`;
-        }
+      // support img tag background
+      const imageElement = el.querySelector(".ee-background-image");
+      if (imageElement) imageElement.style.top = `${y}px`;
     }
+  }
 })();
